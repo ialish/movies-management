f409c8bc7ac71ea494c939fb246322a2cc4d5745
Eyal <eyal.shaar@gmail.com>
2020-08-18 20:07:42 +0300

    Movie Picture

--- routes/movieData.js
+++ routes/movieData.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const getMovie = require('../controllers/movie/getMovie');
+const getMovies = require('../models/readJSON');
 
 router.get('/:movieId', async function(req, res, next) {
 	if (!req.session.admin && !req.session.user) {
 		const message = 'Unauthorized Access';
 		res.render('alert', { message });
 	} else {
-		const movieId = req.params.movieId;
-		const movie = await getMovie(movieId);
+		const movieId = parseInt(req.params.movieId);
+		let movie = {};
+		if (movieId < 250) {
+			movie = await getMovie(movieId);
+		} else {
+			const filename = 'data/newMovies.json';
+			const movies = await getMovies(filename);
+			movie = movies.find(movie => movie.id === movieId);
+		};
 		res.render('movieData', { movie });
 	};
 });
